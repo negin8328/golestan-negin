23592845a4870e27ee79b60c4fd21d324e1c8e16
negin8328 <moradinegin995@gmail.com>
2024-06-06 10:31:26 +0330

    version1

---
+++ goli/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      mobile: "0px",
+      desktop: "1024px",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
